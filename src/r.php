211e0cc7d6ca9d7e9b5758f14eb509fac34f2494
Aubrey Portwood <code@aubreypwd.com>
2022-02-25 12:53:35 -0700

    Allow string.
    E.g. r( '100,RTABLE,RNSCREEN=Something New', [ 'hi' ] );

--- src/r.php
+++ src/r.php
@@ -12,7 +12,9 @@ if ( file_exists( __DIR__ . '/../vendor/autoload.php' ) ) {
 
 	require_once __DIR__ . '/../vendor/autoload.php';
 
-	Kint\Renderer\RichRenderer::$theme = 'solarized-dark.css';
+	if ( class_exists( 'Kint\Renderer\RichRenderer' ) ) {
+		Kint\Renderer\RichRenderer::$theme = 'solarized-dark.css';
+	}
 }
 
 define( 'RSHOW', 100 );
@@ -30,6 +32,7 @@ define( 'RTABLE', 1100 );
 define( 'RSEP', 1200 );
 define( 'RNSCREEN', 1300 );
 define( 'RLABEL', 1400 );
+define( 'RCLEARALL', 1400 ); // https://spatie.be/docs/ray/v1/usage/framework-agnostic-php-project#clearing-everything-including-history
 
 if ( ! function_exists( 'r' ) && function_exists( 'ray' ) ) {
 
@@ -41,64 +44,103 @@ if ( ! function_exists( 'r' ) && function_exists( 'ray' ) ) {
 	 * @param  array $opts
 	 * @param  mixed $vars
 	 */
-	function r( array $opts = [], ...$vars ) {
+	function r( $opts = [], ...$vars ) {
+
+		if ( ! is_array( $opts ) && ! is_string( $opts ) ) {
+			throw new \InvalidArgumentException( '$opts must be a string or an array.' );
+		}
+
+		if ( is_string( $opts ) ) {
+
+			$opts = explode( ',', $opts );
+
+			foreach ( $opts as $opt_key => $opt_value ) {
+
+				if ( stristr( $opt_value, '=' ) ) {
+
+					$opt_config = explode( '=', $opt_value );
+					$constant   = strtoupper( trim( $opt_config[0] ) );
+
+					if ( ! defined( $constant ) ) {
+						continue;
+					}
+
+					$opts[ abs( (int) constant( $constant ) ) ] = trim( $opt_config[1] ?? '' );
+
+					unset( $opts[ $opt_key ] );
+				}
+			}
+
+			foreach ( $opts as $opt_key => $opt_value ) {
+
+				$constant = strtoupper( trim( $opt_value ) );
+
+				if ( ! defined( $constant ) ) {
+					continue;
+				}
+
+				$opts[ $opt_key ] = abs( (int) constant( $constant ) );
+			}
+		}
 
 		$ray   = null;
 		$color = '';
 		$size  = '';
 
-		d( $opts, $vars );
+		if ( in_array( RCLEARALL, $opts ) ) {
+			ray()->clearAll();
+		}
 
-		if ( in_array( RNSCREEN, $opts, true ) ) {
+		if ( in_array( RNSCREEN, $opts ) ) {
 			ray()->newScreen();
 		} elseif (
-			in_array( RNSCREEN, array_keys( $opts ), true ) &&
+			in_array( RNSCREEN, array_keys( $opts ) ) &&
 			is_string( $opts[ RNSCREEN ] )
 		) {
 			ray()->newScreen( $opts[ RNSCREEN ] );
 		}
 
-		if ( in_array( RCLEAR, $opts, true ) ) {
+		if ( in_array( RCLEAR, $opts ) ) {
 			ray()->clearScreen();
 		}
 
-		if ( in_array( RSHOW, $opts, true ) ) {
+		if ( in_array( RSHOW, $opts ) ) {
 			ray()->showApp();
 		}
 
-		if ( in_array( RLARGE, $opts, true ) ) {
+		if ( in_array( RLARGE, $opts ) ) {
 			$size = 'large';
 		}
 
-		if ( in_array( RSMALL, $opts, true ) ) {
+		if ( in_array( RSMALL, $opts ) ) {
 			$size = 'small';
 		}
 
-		if ( in_array( RGREEN, $opts, true ) ) {
+		if ( in_array( RGREEN, $opts ) ) {
 			$color = 'green';
 		}
 
-		if ( in_array( RORANGE, $opts, true ) ) {
+		if ( in_array( RORANGE, $opts ) ) {
 			$color = 'orange';
 		}
 
-		if ( in_array( RRED, $opts, true ) ) {
+		if ( in_array( RRED, $opts ) ) {
 			$color = 'red';
 		}
 
-		if ( in_array( RBLUE, $opts, true ) ) {
+		if ( in_array( RBLUE, $opts ) ) {
 			$color = 'blue';
 		}
 
-		if ( in_array( RPURPLE, $opts, true ) ) {
+		if ( in_array( RPURPLE, $opts ) ) {
 			$color = 'purple';
 		}
 
-		if ( in_array( RGRAY, $opts, true ) ) {
+		if ( in_array( RGRAY, $opts ) ) {
 			$color = 'gray';
 		}
 
-		if ( in_array( RTABLE, $opts, true ) && isset( $vars[0] ) ) {
+		if ( in_array( RTABLE, $opts ) && isset( $vars[0] ) ) {
 
 			if ( is_null( $ray ) ) {
 				$ray = ray()->table( $vars[0] );
@@ -117,15 +159,15 @@ if ( ! function_exists( 'r' ) && function_exists( 'ray' ) ) {
 			$ray->$size();
 		}
 
-		if ( in_array( RLABEL, array_keys( $opts ), true ) && is_string( $opts[ RLABEL ] ) ) {
+		if ( in_array( RLABEL, array_keys( $opts ) ) && is_string( $opts[ RLABEL ] ) ) {
 			ray()->label( $opts[ RLABEL ] );
 		}
 
-		if ( in_array( RPAUSE, $opts, true ) ) {
+		if ( in_array( RPAUSE, $opts ) ) {
 			ray()->pause();
 		}
 
-		if ( in_array( RSEP, $opts, true ) ) {
+		if ( in_array( RSEP, $opts ) ) {
 			ray()->separator();
 		}
 
